fedfcb84abf63a2c1912ad95439b95f7389ed2d7
eliskim27 <eliskim27@gmail.com>
2019-12-17 15:33:54 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,95 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
-
-  let imageId = 1 //Enter the id from the fetched image here
-
+  // "global" variables
+  let imageId = 4173
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  const likes = document.getElementById('likes')
+  let imageCommentsContainer = document.getElementById('comments')
+
+  function fetchImage(){
+    fetch(imageURL)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(image){
+      let imageCard = document.getElementById('image_card')
+      let imageActual = document.getElementById('image')
+      let imageName = document.getElementById('name')
+      let imageLikes = document.getElementById('likes')
+      imageActual.src = image.url
+      imageName.innerText = image.name
+      imageLikes.innerText = image.like_count
+      image.comments.forEach(function(comment){
+        let newComment = document.createElement('li')
+        newComment.innerHTML = comment.content
+        imageCommentsContainer.append(newComment)
+      })
+
+    })
+  }
+
+  fetchImage()
+
+
+  let imageLikeBtn = document.getElementById('like_button')
+
+  imageLikeBtn.addEventListener('click', function(){
+    likes.innerText = parseInt(likes.innerText) + 1
+    updateLikesDB()
+  })
+  
+  function updateLikesDB(){
+    fetch(likeURL,{
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: `${imageId}`
+      })
+    })
+  }
+  
+  let commentForm = document.getElementById('comment_form')
+  let commentFormInput = document.getElementById('comment_input')
+  
+  commentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    let newComment = document.createElement('li')
+    let newCommentText = commentFormInput.value
+    newComment.innerText = newCommentText
+    imageCommentsContainer.append(newComment)
+    commentForm.reset()
+    updateCommentsDB(newCommentText)
+  })
+  
+  function updateCommentsDB(newCommentText){
+    fetch(commentsURL,{
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: `${imageId}`,
+        content: `${newCommentText}`
+      })
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
